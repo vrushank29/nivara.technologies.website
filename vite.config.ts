2b3531c8a5666625ec927f972345432ad8823b4f
Vrushank <dvrushank@gmail.com>
2026-05-08 12:33:38 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/nivara.technologies.website/',
+    base: '/',
 
     plugins: [react(), tailwindcss()],
 
